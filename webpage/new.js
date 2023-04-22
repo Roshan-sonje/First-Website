5a682a3cd35a39e66f47cb4f32f0a92693819fb3
Roshan Sonje <91359767+Roshansonje1@users.noreply.github.com>
2023-04-22 10:03:04 +0530

    Add files via upload

---
+++ webpage/new.js
@@ -0,0 +1,19 @@
+// const toggle = document.getElementById('toggleDark');
+// const body = document.querySelector("body");
+
+// toggle.addEventListener("click", () =>{
+//     this.classList.toggle("bi-moon");
+// });
+
+const body = document.querySelector(".light");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+toggle.addEventListener("change", () => {
+    
+    body.classList.toggle("dark");
+    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+
+});
